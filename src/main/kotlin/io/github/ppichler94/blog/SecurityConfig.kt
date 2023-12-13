76bb26947ae20032da3428dbb1041c9f96d1c1c5
Patrick Pichler <ppichler@outlook.at>
2023-12-13 18:57:59 +0100

    feat: implement authentication and logout success handlers

--- src/main/kotlin/io/github/ppichler94/blog/SecurityConfig.kt
+++ src/main/kotlin/io/github/ppichler94/blog/SecurityConfig.kt
@@ -8,7 +8,9 @@ import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.security.web.context.DelegatingSecurityContextRepository
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository
@@ -38,14 +40,15 @@ class SecurityConfig {
                 )
             }
             formLogin {
-                defaultSuccessUrl("/api/user", true)
+                authenticationSuccessHandler = AuthenticationSuccessHandler { _, response, _ -> response.status = 200 }
                 permitAll = true
             }
             exceptionHandling {
                 authenticationEntryPoint = Http403ForbiddenEntryPoint()
             }
             logout {
-                logoutSuccessUrl = "/api/user"
+                logoutSuccessHandler = LogoutSuccessHandler { _, response, _ -> response.status = 200 }
+                permitAll = true
             }
         }
         return http.build()
